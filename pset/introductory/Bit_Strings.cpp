c830a09174628d07f5f4aadd7bb29826e106cde0
Rangon Roy Utsab <rangonroy.rzs17@gmail.com>
2021-07-16 14:16:53 +0600

    add

---
+++ pset/introductory/Bit_Strings.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main ()
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+
+    long long int n, i, r = 1, m = 1000000007;
+    cin >> n;
+    
+    for (i = 1; i <= n; i++) {
+        r = (r*2) % m;
+    }
+    cout << r << endl;
+}
---
+++ pset/introductory/Trailing_Zeros.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+
+ll log5(ll n)
+{
+    ll c = 0;
+    while (n % 5 == 0)
+    {
+        c++;
+        n = n / 5;
+    }
+    return c;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+
+    ll n, ans = 0;
+    cin >> n;
+
+    for (ll i = 5; i <= n; i += 5)
+    {
+        if (i % 5 == 0)
+        {
+            ans += log5(i);
+        }
+    }
+
+    cout << ans << endl;
+}

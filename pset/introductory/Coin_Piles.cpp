f9a7f53babb81e7e730addf41711c4aadc50ec06
Rangon Roy Utsab <rangonroy.rzs17@gmail.com>
2021-07-19 17:39:41 +0600

    formated

--- pset/introductory/Coin_Piles.cpp
+++ pset/introductory/Coin_Piles.cpp
@@ -3,29 +3,23 @@ using namespace std;
 
 using ll = long long int;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    ll t, a, b;
-    cin >> t;
+  ll t, a, b;
+  cin >> t;
 
-    while (t--)
-    {
-        cin >> a >> b;
-        if ((a + b) % 3 == 0)
-        {
-            if (min(a, b) >= ((a + b) / 3.0))
-            {
-                cout << "YES\n";
-            }
-            else
-                cout << "NO\n";
-        }
-        else
-            cout << "NO\n";
-    }
+  while (t--) {
+    cin >> a >> b;
+    if ((a + b) % 3 == 0) {
+      if (min(a, b) >= ((a + b) / 3.0)) {
+        cout << "YES\n";
+      } else
+        cout << "NO\n";
+    } else
+      cout << "NO\n";
+  }
 
-    return 0;
+  return 0;
 }
